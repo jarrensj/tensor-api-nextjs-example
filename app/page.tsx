2f903b48d0a017725cf9bae156d5ce46eeb427a2
Jarren San Jose <sanjose.jarren@gmail.com>
2024-01-09 17:53:11 -0800

    tensor api nextjs example

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link'
 import Tensorians from '@/components/Tensorians'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-2">sup</h1>
+      <h1 className="text-4xl font-bold mb-2">tensor api nextjs example</h1>
+      <p className="text-xl text-gray-700 mb-6">by&nbsp;
+        <Link href="https://jarrensj.com">
+          <span className="text-blue-500 hover:underline">jarrensj</span>
+        </Link>
+      </p>
       <Tensorians />
     </main>
   )
